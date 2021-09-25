059aaa425645c223cce7be396bd6291fdb6d7ffc
dinafauzyh <dfauziyah18@gmail.com>
2021-09-26 04:16:15 +0700

    Add jadwal baru sudah terinput dengan statusnya

--- app/Jadwal.php
+++ app/Jadwal.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Jadwal extends Model
 {
     protected $table = 'jadwal';
-    protected $fillable = ['jam', 'harga', 'lapangan_id'];
+    protected $fillable = ['jam', 'harga', 'lapangan_id', 'status'];
 }
